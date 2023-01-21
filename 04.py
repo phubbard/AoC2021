2fc485a2bfbe13505d62b3991ee06ab79400e147
Brad Hyslop @ mimic <bhyslop@scaleinvariant.org>
2023-01-21 15:02:54 -0800

    start-on-4

---
+++ 04.py
@@ -0,0 +1,17 @@
+from copy import deepcopy
+from utils import make_2d_array, get_column, get_data_lines
+
+
+def part_one(data_2d):
+    print("Not started!")
+
+def part_two(data_2d):
+    print("Not started!")
+
+
+if __name__ == '__main__':
+
+    sample, full = get_data_lines(4)
+    for step in [sample, full]:
+        part_one(step)
+        part_two(step)
