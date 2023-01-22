f25eccf8ddf284a587680393b428dbc0fd74f859
Brad Hyslop @ mimic <bhyslop@scaleinvariant.org>
2023-01-22 15:39:20 -0800

    Solution validated.

--- p07.py
+++ p07.py
@@ -1,28 +1,55 @@
 from utils import *
-import numpy as np
-
-
-def fuel_allocation(data1d, location):
-    return np.abs(np.array(data1d) - location).sum()
-
-def part_one(data1d):
-    num_pts = len(data1d)
-    average = sum(data1d) / num_pts
-    print(f'{average=} {num_pts=}')
-
-    total_fuel = 0
-    for crab in data1d:
-        total_fuel += abs(crab - average)
-    print(f'{total_fuel=}')
-
 
 if __name__ == '__main__':
     sample, full = get_data_lines(7)
-    for data in [sample, full]:
+    for tag, data, best_linear_cost, best_gauss_cost in [
+                ("sample", sample,     37,       168),
+                ("real",   full,   351901, 101079875),
+            ]:
         parsed = data[0].split(',')
         parsed = [int(x) for x in parsed]
-        part_one(parsed)
-        print(np.array(parsed).mean())
-        for i in range(7):
-            print(i, fuel_allocation(parsed,i))
+        print(f"{tag}: count is {len(parsed)}")
+        min_extent = min(parsed)
+        max_extent = max(parsed)
+        print(f"{tag}: {min_extent=}")
+        print(f"{tag}: {max_extent=}")
+
+        min_linear_cost   = None
+        min_linear_target = None
+        min_gauss_cost    = None
+        min_gauss_target  = None
+        for test_target in range(min_extent, max_extent + 1):
+            total_linear_cost = 0
+            total_gauss_cost = 0
+
+            for crab_start in parsed:
+                distance = abs(crab_start - test_target)
+                linear_cost = distance
+                total_linear_cost += linear_cost
+                gauss_cost = int(distance * (distance + 1) / 2)
+                total_gauss_cost += gauss_cost
+                # print(f"{crab_start=} yields {total_linear_cost=}")
+            if min_linear_cost is None or total_linear_cost < min_linear_cost:
+                min_linear_cost = total_linear_cost
+                min_linear_target = test_target
+                # print(f"at {min_linear_target=} better cost is {min_linear_cost=}")
+            if min_gauss_cost is None or total_gauss_cost < min_gauss_cost:
+                min_gauss_cost = total_gauss_cost
+                min_gauss_target = test_target
+                # print(f"at {min_gauss_target=} better cost is {min_gauss_cost=}")
+
+        found_best_linear_cost   = min_linear_cost
+        found_best_linear_target = min_linear_target
+
+        found_best_gauss_cost   = min_gauss_cost
+        found_best_gauss_target = min_gauss_target
+
+        print(f"Lowest linear cost is {found_best_linear_cost} found at {found_best_linear_target}")
+        print(f"Lowest gauss cost is {found_best_gauss_cost} found at {found_best_gauss_target}")
+        if found_best_linear_cost != best_linear_cost:
+            raise Exception("YOU SHALL NOT PASS")
+        if found_best_gauss_cost != best_gauss_cost:
+            raise Exception("YOU SHALL NOT GAUSS PASS")
+
+
 
