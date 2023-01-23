2ef54959de8671d1955fadea40985ffe91df93e2
Paul Hubbard <paul_hubbard@intuit.com>
2023-01-22 16:40:09 -0800

    part 1 done.

--- p08.py
+++ p08.py
@@ -1,18 +1,17 @@
 from utils import *
+from collections import Counter
 
-num_lit = {
-    0: 6, 1: 2, 2: 6, 3: 6, 4: 4, 5: 6, 6: 7, 7: 3, 8: 7, 9: 6
-}
 
 if __name__ == '__main__':
     sample, full = get_data_lines(8)
     for raw in [sample, full]:
         unique = set()
+        tracking = Counter()
         for cur_line in raw:
             # Input | output
             tokens = cur_line.split('|')
             output_tokens = tokens[1].strip().split(' ')
             for item in output_tokens:
                 if len(item) in [2, 3, 4, 7]:
-                    unique.add(item)
-        print(f'{len(unique)=}')
+                    tracking.update([item])
+        print(f'{tracking.total()=}')
