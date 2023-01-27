ba1ddbeed139dd779fedf9e44fdb822bd4db2e8f
Paul Hubbard <paul_hubbard@intuit.com>
2023-01-26 20:32:08 -0800

    chris nailed 10 and now added data for 11

--- p10.py
+++ p10.py
@@ -1,15 +1,31 @@
 from utils import *
 
+
 scoring_table = {
     ')': 3, ']': 57, '}': 1197, '>': 25137
 }
 
 
 def part_one(datalines):
-    pass
+    for line in datalines:
+        counts = {}
+        cl_map = {'>': '<', ')': '(', '}': '{', ']': '['}
+        open_chars = ['<', '{', '[', '(']
+        for x in open_chars:
+            counts[x] = 0
 
+        for character in line:
+            if character in open_chars:
+                counts[character] += 1
+            else:
+                open_char = cl_map[character]
+                if counts[open_char] <= 0:
+                    print(f'Error, got {character}')
+                else:
+                    counts[open_char] -= 1
+        print(f'{counts=}')
 
 if __name__ == '__main__':
     sample, full = get_data_lines(10)
-    for cur_dataset in [sample, full]:
+    for cur_dataset in [sample]:
         part_one(cur_dataset)
