a5175724067c11ce2d6e275924ed1f95d2b79d59
Brad Hyslop @ mimic <bhyslop@scaleinvariant.org>
2023-01-29 14:47:15 -0800

    perms-not-combos

--- p14_brad.py
+++ p14_brad.py
@@ -1,7 +1,8 @@
 
+
 import collections
 
-from utils import get_data_lines
+from utils import get_data_lines, find_permutations
 
 def log(string):
     print(f"P14: {string}")
@@ -29,14 +30,17 @@ def run_bigger_rules(polymer, rules, chunk_size):
     chunks = []
     while len(polymer) >= chunk_size:
         chunk = polymer[0:chunk_size]
-        polymer = polymer[chunk_size:]
+        polymer = polymer[chunk_size - 1:]
         chunks.append(chunk)
-    chunks.append(polymer)
+    for remain_index in range(len(polymer) - 1):
+        remains = polymer[remain_index] + polymer[remain_index + 1]
+        chunks.append(remains)
 
     new_polymer = first_character
     for chunk in chunks:
         result = rules.get(chunk, chunk)
         new_polymer += result[1:]
+        log(f"For {chunk=} we found {result=} leading to next {new_polymer=}")
     return new_polymer
 
 
@@ -46,27 +50,33 @@ full_distinct = {'H': True, 'K': True, 'P': True, 'S': True, 'B': True, 'V': Tru
 
 if __name__ == '__main__':
     sample, full = get_data_lines(14)
-    for dataset, expected in [
-                (sample, 2188189693529),
-            #    (full, -1),
+    for dataset, dumb_dict, expected in [
+                (sample, sample_distinct, 2188189693529),
+            #    (full, full_distinct, -1),
                 ]:
         sample_template = dataset[0]
         sample_pi = {}
+        new_codebook = {}
         for idx, value in enumerate(dataset[2:]):
             tokens = value.split(' -> ')
             sample_pi[tokens[0]] = tokens[1]
+            new_codebook[tokens[0]] = tokens[0][0] + tokens[1] + tokens[0][1]
             
-        for manual_permute in [
-                'CCC', 'CCH', 'CCB', 'CCN',
-                'CHC', 'CHH', 'CHB', 'CHN',
-               ]:
+        order = 3
+
+        for manual_permute in find_permutations(dumb_dict.keys(), order):
             resultant = run_rules(manual_permute, sample_pi)
-            sample_pi[manual_permute] = resultant
+            manual = ''.join(manual_permute)
+            log(f"We think {manual=} resolves to {resultant=}")
+            new_codebook[manual] = resultant
 
         for step in range(3):
+            log(f"STARTING WITH {sample_template=}")
             old_template = run_rules(sample_template, sample_pi)
-            new_template = run_bigger_rules(sample_template, sample_pi, 3)
-            log(f"{step=}  {old_template=} {new_template=}")
+            new_template = run_bigger_rules(sample_template, new_codebook, order)
+            log(f"{step=}")
+            log(f"      {old_template=}")
+            log(f"      {new_template=}")
 
             sample_template = old_template
 
