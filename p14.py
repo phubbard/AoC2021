d603d2a4f98f5031d45b8ed4f54d0555e7fad25a
Paul Hubbard <paul_hubbard@intuit.com>
2023-01-29 14:29:15 -0800

    building permutations

--- p14.py
+++ p14.py
@@ -1,23 +1,37 @@
 from utils import get_data_lines, find_permutations
 
 
-def run_rules(polymer, rules):
-    for rule in rules:
-        loc = polymer.find(rule[0])
-        if loc >= 0:
-            print(f'Applying {rule=} to {polymer=}')
-            polymer = polymer[:loc] + rule[1] + polymer[loc:]
-
-    return polymer
 
 
 if __name__ == '__main__':
     sample, full = get_data_lines(14)
     sample_template = sample[0]
-    sample_pi = []
+    sample_pt = []
     for idx, value in enumerate(sample[2:]):
         tokens = value.split(' -> ')
-        sample_pi.append((tokens[0], tokens[1],))
-    answ = run_rules(sample_template, sample_pi)
+        sample_pt.append((tokens[0], tokens[1],))
 
-    assert answ == 'NCNBCHB'
+    full_pt = []
+    for value in full[2:]:
+        tokens = value.split(' -> ')
+        full_pt.append((tokens[0], tokens[1]))
+
+    unique_chars = set()
+    for rule in sample_pt:
+        unique_chars.update(rule[0][0])
+        unique_chars.update(rule[0][1])
+        unique_chars.update(rule[1])
+
+    print(f'{len(list(unique_chars))=} {list(unique_chars)=}')
+    perms = find_permutations(unique_chars, 4)
+    print(f'{len(list(perms))=}')
+
+    unique_chars = set()
+    for rule in full_pt:
+        unique_chars.update(rule[0][0])
+        unique_chars.update(rule[0][1])
+        unique_chars.update(rule[1])
+
+    print(f'{len(list(unique_chars))=} {list(unique_chars)=}')
+    perms = find_permutations(unique_chars, 8)
+    print(f'{len(list(perms))=:,}')
