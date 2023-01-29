ff2f90809ee85f62f599e5daebe887e30f077db0
Brad Hyslop @ mimic <bhyslop@scaleinvariant.org>
2023-01-29 14:31:30 -0800

    partial work on chunking, merging next with paul.

--- p14_brad.py
+++ p14_brad.py
@@ -24,7 +24,23 @@ def run_rules(polymer, rules):
 
     return new_polymer
 
-sample_distinct={'C': True, 'H': True, 'B': True, 'N': True}
+def run_bigger_rules(polymer, rules, chunk_size):
+    first_character = polymer[0]
+    chunks = []
+    while len(polymer) >= chunk_size:
+        chunk = polymer[0:chunk_size]
+        polymer = polymer[chunk_size:]
+        chunks.append(chunk)
+    chunks.append(polymer)
+
+    new_polymer = first_character
+    for chunk in chunks:
+        result = rules.get(chunk, chunk)
+        new_polymer += result[1:]
+    return new_polymer
+
+
+sample_distinct = {'C': True, 'H': True, 'B': True, 'N': True}
 
 full_distinct = {'H': True, 'K': True, 'P': True, 'S': True, 'B': True, 'V': True, 'C': True, 'F': True, 'N': True, 'O': True}
 
@@ -32,7 +48,7 @@ if __name__ == '__main__':
     sample, full = get_data_lines(14)
     for dataset, expected in [
                 (sample, 2188189693529),
-                (full, -1),
+            #    (full, -1),
                 ]:
         sample_template = dataset[0]
         sample_pi = {}
@@ -40,15 +56,25 @@ if __name__ == '__main__':
             tokens = value.split(' -> ')
             sample_pi[tokens[0]] = tokens[1]
             
-        for step in range(40):
-            log(f"{step=}")
-            sample_template = run_rules(sample_template, sample_pi)
+        for manual_permute in [
+                'CCC', 'CCH', 'CCB', 'CCN',
+                'CHC', 'CHH', 'CHB', 'CHN',
+               ]:
+            resultant = run_rules(manual_permute, sample_pi)
+            sample_pi[manual_permute] = resultant
+
+        for step in range(3):
+            old_template = run_rules(sample_template, sample_pi)
+            new_template = run_bigger_rules(sample_template, sample_pi, 3)
+            log(f"{step=}  {old_template=} {new_template=}")
+
+            sample_template = old_template
 
         counter = collections.Counter(sample_template)
         sorted_out = counter.most_common()
         log(f"{sorted_out=}")
         score = sorted_out[0][1] - sorted_out[-1][1]
         log(f"{score=}")
-        assert score == expected
+        # assert score == expected
 
 
