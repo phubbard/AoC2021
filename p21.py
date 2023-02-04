77cf93bc9e5ffd675b5446cce5cad0b978bb8ea7
Paul Hubbard <paul_hubbard@intuit.com>
2023-02-03 19:12:50 -0800

    part one done

---
+++ p21.py
@@ -0,0 +1,51 @@
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Player:
+    pos: int = 1
+    score: int = 0
+
+    def move(self, count):
+        self.pos = ((self.pos - 1  + count) % 10) + 1
+        self.score += self.pos
+
+
+class Die:
+    def __init__(self):
+        self.rolls = 0
+        self.current = 0
+
+    def __repr__(self):
+        return f'{self.rolls=} {self.current=}'
+
+
+    def roll(self):
+        self.rolls += 1
+        self.current = (self.current + 1) % 100
+        return self.current
+
+    def triple_roll(self):
+        return self.roll(), self.roll(), self.roll()
+
+
+def part_one():
+    p1 = Player(pos=6)
+    p2 = Player(pos=7)
+    die = Die()
+    max_score = 0
+    while max_score < 1000:
+        scores = sum(die.triple_roll())
+        p1.move(scores)
+        if p1.score >= 1000:
+            break
+        scores = sum(die.triple_roll())
+        p2.move(scores)
+        max_score = max(p1.score, p2.score)
+
+    print(f'{p1=} {p2=} {die=}')
+    print(f'Score {min(p1.score, p2.score) * die.rolls}')
+
+if __name__ == '__main__':
+    part_one()
