06138c7e072b34d53b4068448dcb0bd719adc444
Paul Hubbard <paul_hubbard@intuit.com>
2023-02-05 11:56:39 -0800

    merge hell

--- p21.py
+++ p21.py
@@ -6,10 +6,12 @@ from dataclasses import dataclass
 class Player:
     pos: int = 1
     score: int = 0
+    universes: int = 0
 
     def move(self, count):
         self.pos = ((self.pos - 1  + count) % 10) + 1
         self.score += self.pos
+        self.universes += 3
 
 
 class Die:
@@ -20,7 +22,6 @@ class Die:
     def __repr__(self):
         return f'{self.rolls=} {self.current=}'
 
-
     def roll(self):
         self.rolls += 1
         self.current = (self.current + 1) % 100
@@ -28,6 +29,12 @@ class Die:
 
     def triple_roll(self):
         return self.roll(), self.roll(), self.roll()
+
+
+class DiracDie(Die):
+    def roll(self):
+        self.rolls += 1
+        self.current = (self.current + 1) % 3
 
 
 def part_one():
@@ -46,6 +53,22 @@ def part_one():
 
     print(f'{p1=} {p2=} {die=}')
     print(f'Score {min(p1.score, p2.score) * die.rolls}')
+
+
+def part_two():
+    p1 = Player(pos=6)
+    p1 = Player(pos=7)
+    die = DiracDie()
+    max_score = 0
+    while max_score < 21:
+        scores = sum(die.triple_roll())
+        p1.move(scores)
+        if p1.score >= 21:
+            break
+        scores = sum(die.triple_roll())
+        p2.move(scores)
+        max_score = max(p1.score, p2.score)
+
 
 if __name__ == '__main__':
     part_one()
